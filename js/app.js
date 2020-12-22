cecfa9c664f98699d1142018fe9e012220b0a4d5
Edu <edugacv@gmail.com>
2020-12-22 10:32:36 +0100

    Exercice done

--- js/app.js
+++ js/app.js
@@ -5,3 +5,29 @@
     "contBcg-3",
     "contBcg-4"
   ];
+
+  let rightBtn = document.querySelector('.btn-right')
+  let leftBtn = document.querySelector('.btn-left')
+  let containerImage = document.querySelector('.img-container')
+
+  let count = 0;
+
+  rightBtn.addEventListener('click', function(){
+    if(count < pictures.length -1){
+      count++
+      containerImage.style.setProperty("background", `url('../img/${pictures[count]}.jpeg') center/cover fixed no-repeat`)
+    } else {
+      count = 0
+      containerImage.style.setProperty("background", `url('../img/${pictures[count]}.jpeg') center/cover fixed no-repeat`)
+    }
+    
+  })
+
+  leftBtn.addEventListener('click', function(){
+    if(count == 0){
+      count = 5
+    }
+    count--
+    containerImage.style.setProperty("background", `url('../img/${pictures[count]}.jpeg') center/cover fixed no-repeat`)
+      
+  })
